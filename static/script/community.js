6e4b7bba451574c17f767dafc02eddd3f9b412dd
salterclub <102706483+salterclub@users.noreply.github.com>
2022-11-18 18:00:50 -0500

    yes
    eys

---
+++ static/script/community.js
@@ -0,0 +1,51 @@
+/*
+  _____                   _                _     _                                                                      
+ |  __ \                 | |              | |   | |                                                                     
+ | |__) |   ___    _ __  | |_    ___    __| |   | |__    _   _                                                          
+ |  ___/   / _ \  | '__| | __|  / _ \  / _` |   | '_ \  | | | |                                                         
+ | |      | (_) | | |    | |_  |  __/ | (_| |   | |_) | | |_| |                                                         
+ |_|       \___/  |_|     \__|  \___|  \__,_|   |_.__/   \__, |                                                         
+                                                          __/ |                                                         
+                                                         |___/                                                          
+                                _     _                     _       _   _          _                               _    
+     /\                        | |   | |                   | |     | \ | |        | |                             | |   
+    /  \     _ __ ___     ___  | |_  | |__    _   _   ___  | |_    |  \| |   ___  | |_  __      __   ___    _ __  | | __
+   / /\ \   | '_ ` _ \   / _ \ | __| | '_ \  | | | | / __| | __|   | . ` |  / _ \ | __| \ \ /\ / /  / _ \  | '__| | |/ /
+  / ____ \  | | | | | | |  __/ | |_  | | | | | |_| | \__ \ | |_    | |\  | |  __/ | |_   \ V  V /  | (_) | | |    |   < 
+ /_/    \_\ |_| |_| |_|  \___|  \__| |_| |_|  \__, | |___/  \__|   |_| \_|  \___|  \__|   \_/\_/    \___/  |_|    |_|\_\
+                                               __/ |                                                                    
+                                              |___/                                                                     
+*/
+function community(app) {
+    app.search.title.style.display = 'block';
+    app.search.title.textContent = 'Community';
+    app.search.input.style.display = 'none';
+    app.main.support = app.createElement(
+        'div', 
+        [
+            app.createElement('section', [
+                app.createElement('p', 'You are being taken to the roblox zone? (https://www.roblox.com).', {
+                        style: {
+                            'margin-bottom': '0'
+                        }
+                    }),
+                    app.createElement('p', 'Are you sure you want to <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Male_adult_masturbation.gif/1000px-Male_adult_masturbation.gif">proceed</a>?', {
+                        style: {
+                            'margin-bottom': '0'
+                        }
+                    }),
+            ], {
+                class: 'data-section'
+            }),
+            
+        ]);
+        app.search.back.style.display = 'inline';
+    app.search.back.setAttribute(
+        'onclick',
+        '(' + (function(){
+            window.location.hash = '';
+        }).toString() + ')();'
+    )
+};
+
+export { community };
